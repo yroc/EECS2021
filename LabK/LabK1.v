d9c69b24da2aa88201396599db90983383f4a75b
Cory Sand <yrocsand@gmail.com>
2016-11-24 17:38:10 -0500

    Step 11: Declare, init, and display reg variables one, two, and three

--- LabK/LabK1.v
+++ LabK/LabK1.v
@@ -1,6 +1,9 @@
 module LabK1;
    reg [31:0] x;
-
+   reg        one;
+   reg [1:0]  two;
+   reg [2:0]  three;
+   
    initial
      begin
         $display("time = %5d, x = %b", $time, x);
@@ -8,6 +11,13 @@ module LabK1;
         $display("time = %5d, x = %b", $time, x);
         x = x + 2;
         $display("time = %5d, x = %b", $time, x);
+
+        // Assign and display varaibles one, two, three
+        one = &x;               // and reduction
+        two = x[1:0];           // part-selection
+        three = {one, two};     // concatenation
+        $display("one=%b, two=%b, three=%b", one, two, three);
+        
         $finish;
      end
 endmodule // LabK1
